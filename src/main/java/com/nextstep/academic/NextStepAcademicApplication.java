539224da2566071bb3fe5f546bf4ef7a5bc5f28b
Adithya Ekanayaka <adithyasean@outlook.com>
2025-01-01 21:41:16 +0530

    initial commit

---
+++ src/main/java/com/nextstep/academic/NextStepAcademicApplication.java
@@ -0,0 +1,13 @@
+package com.nextstep.academic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextStepAcademicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextStepAcademicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nextstep/academic/NextStepAcademicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextstep.academic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextStepAcademicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
